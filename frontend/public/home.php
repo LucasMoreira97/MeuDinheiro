9b2005eee862a9776953c88f168850ac890bf30a
Lucas Moreira <l-moreira@outloook.com.br>
2024-09-07 02:02:04 -0300

    Ajustes no front end

--- frontend/public/home.php
+++ frontend/public/home.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
+// session_start();
 
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("Location: index.php");
-    exit;
-}
+// if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+//     header("Location: index.php");
+//     exit;
+// }
 ?>
 
 <!DOCTYPE html>
@@ -21,36 +21,38 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 
 <body>
 
+    <div class="home">
+        <div class="page" id="home-page"></div>
 
-    <div class="page" id="home-page"></div>
+        <div class="bottom-menu">
+            <div class="menu-item active" id="general">
+                <img src="../src/assets/money-mangement.png" alt="">
+                <span>Geral</span>
+            </div>
 
-    <div class="bottom-menu">
-        <div class="menu-item active" id="general">
-            <img src="../src/assets/money-mangement.png" alt="">
-            <span>Geral</span>
+
+            <div class="menu-item" id="income" onclick="home.addIncomePage()">
+                <img src="../src/assets/income.png">
+                <span>Receitas</span>
+            </div>
+
+
+            <div class="menu-item" id="expenses" onclick="home.addExpensePage()">
+                <img src="../src/assets/expenses-in-business.png">
+                <span>Despesas</span>
+            </div>
+
+            <div class="menu-item" id="investments">
+                <img src="../src/assets/currency.png">
+                <span>Investimentos</span>
+            </div>
+
+            <div class="menu-item" id="profile">
+                <img src="../src/assets/profile.png">
+                <span>Perfil</span>
+            </div>
         </div>
 
-
-        <div class="menu-item" id="income" onclick="home.addIncomePage()">
-            <img src="../src/assets/income.png">
-            <span>Receitas</span>
-        </div>
-
-
-        <div class="menu-item" id="expenses" onclick="home.addExpensePage()">
-            <img src="../src/assets/expenses-in-business.png">
-            <span>Despesas</span>
-        </div>
-
-        <div class="menu-item" id="investments">
-            <img src="../src/assets/currency.png">
-            <span>Investimentos</span>
-        </div>
-
-        <div class="menu-item" id="profile">
-            <img src="../src/assets/profile.png">
-            <span>Perfil</span>
-        </div>
     </div>
 
     <script src="./js/jquery-3.6.0.min.js"></script>
